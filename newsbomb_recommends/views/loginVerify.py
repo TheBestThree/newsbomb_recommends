445ab6c98954e060a51be555d8d402493e3210f0
zhoujiafeng <1092803192@qq.com>
2015-12-06 19:03:00 +0800

    hhh

--- newsbomb_recommends/views/loginVerify.py
+++ newsbomb_recommends/views/loginVerify.py
@@ -1,10 +1,37 @@
+import requests
+from couchbase.bucket import Bucket
 __author__ = 'zjf'
 
 
-def usernameIsValue(json):
-    getUsernameJson = json.loads(json)
-    getUsername = getUsernameJson["username"]
-    if getUsername == "user":
-        return True
-    else:
-        return False
+def send_simple_message(username):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxc140f8236f954551bc5f270901158c43.mailgun.org/messages",
+        auth=("api", "key-df9ae46da72fb2eb9c377dfa26664014"),
+        data={"from": "Excited User <mailgun@sandboxc140f8236f954551bc5f270901158c43.mailgun.org>",
+              "to": [username],
+              "subject": "Welcome to NewsBomb",
+              "text": "Testing some Mailgun awesomness!"})
+
+def usernameIsValue(username):
+    print username
+    try:
+        bucket = Bucket('couchbase://localhost/default')
+        rv = bucket.get(username)
+        if rv is not None:
+            return True
+
+    except Exception as e:
+        print "not found"
+        send_simple_message(username)
+
+    #print rv["value"]
+    #print rv.value
+    # getUsernameJson = json.loads(username)
+    #getUsername = username["username"]
+
+
+#if __name__ == '__main__':
+#   send_simple_message("max.meng@appartner.cn")
+
+
+
